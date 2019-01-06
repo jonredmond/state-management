b3fbe43a46846c60b208f9168bf9da8128ac5272
Jon Redmond <jredmond@sapient.com>
2019-01-06 14:00:52 +0000

    complete step-02

--- src/index.js
+++ src/index.js
@@ -5,11 +5,28 @@ import { hot } from 'react-hot-loader';
 import { Button, Counter } from './components';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+
+  increaseCount = () => {
+    const { count } = this.state;
+
+    this.setState({
+      count: count + 1
+    });
+  };
+
   render() {
+    const { count } = this.state;
+
     return (
       <Fragment>
-        <Counter count={0} />
-        <Button onCLick={() => {}} />
+        <Counter count={count} />
+        <Button onClick={this.increaseCount} />
       </Fragment>
     );
   }
